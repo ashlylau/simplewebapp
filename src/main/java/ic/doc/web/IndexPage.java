9d49369a35b1a2fdf21367097786e889120d5683
Ashly Lau <al5217@sprite19.doc.ic.ac.uk>
2019-01-24 13:17:05 +0000

    Changed style of webapp to use Google style

--- src/main/java/ic/doc/web/IndexPage.java
+++ src/main/java/ic/doc/web/IndexPage.java
@@ -13,26 +13,28 @@ public class IndexPage implements Page {
         // Header
         writer.println("<html>");
         writer.println("<head><title>Welcome</title></head>");
+        writer.println("<link rel=\"stylesheet\" href=\"https://www.google.com/css/maia.css\">");
+//        writer.println("<link rel=\"stylesheet\" href=\"https://unpkg.com/purecss@1.0.0/build/pure-min.css\" integrity=\"sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w\" crossorigin=\"anonymous\">");
         writer.println("<body>");
         writer.println("<style>" +
-                "body { background-color: #5dade2; }");
+                "body { }");
 
         writer.println("</style>");
         // Content
         writer.println(
 
-                "<h1><center><font size=\"10\">Welcome to our WebApp!!</font></center></h1>" +
-                        "<p><i><center><font face=\"Bedrock\" size=\"6\">Enter your query in the box below:</font></center></i> " +
+                "<h1><center><font size=\"10\">Group 5 WebApp</font></center></h1>" +
+                        "<p><center><font face=\"Bedrock\" size=\"6\">Enter your query in the box below:</font></center> " +
                         "<form>" +
                         "<br>" +
                         "<center><input type=\"text\" name=\"q\" placeholder=\"Search..\"/></center>" +
                         "<br><br>" +
-                        "<center><input type=\"radio\" name=\"type\" value = \"html\" /> <b><font size=\"4\">View as HTML</font></center></b>" +
+                        "<center><input type=\"radio\" name=\"type\" value = \"html\" /> <font size=\"4\">View as HTML</font></center>" +
                         "<br>" +
-                        "<center><input type=\"radio\" name=\"type\" value = \"markdown\" /> <b><font size=\"4\">Download as Markdown</font></center></b>" +
+                        "<center><input type=\"radio\" name=\"type\" value = \"markdown\" /> <font size=\"4\">Download as Markdown</font></center>" +
                         "<br>" +
-                        "<center><input type=\"radio\" name=\"type\" value = \"pdf\" /> <b><font size=\"4\">Download as PDF</font></center></b>" +
-                        "<center><br><br><input type=\"submit\"></center>" +
+                        "<center><input type=\"radio\" name=\"type\" value = \"pdf\" /> <font size=\"4\">Download as PDF</font></center>" +
+                        "<center><br><br><br><input type=\"submit\"></center>" +
                         "</form>" +
                         "</p>");
         

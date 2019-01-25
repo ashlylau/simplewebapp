907bb11aa7f82b9a363830f38bd9548543edc273
pm3617 <pm3617@ic.ac.uk>
2019-01-25 16:00:47 +0000

    fixed style

--- src/main/java/ic/doc/web/IndexPage.java
+++ src/main/java/ic/doc/web/IndexPage.java
@@ -32,9 +32,9 @@ public class IndexPage implements Page {
                         "<br>" +
                         "<center><input type=\"radio\" name=\"type\" value = \"markdown\" /> <b><font size=\"4\">Download as Markdown</font></center></b>" +
                         "<br>" +
-                        "<center><input type=\"radio\" name=\"type\" value = \"pdf\" /> <font size=\"4\">Download as PDF\n</font></center" +
-                        "<br>" +
-                        "<center><input type=\"radio\" name=\"type\" value = \"latex\" /> <font size=\"4\">Download as LaTeX</font></center>" +
+                        "<center><input type=\"radio\" name=\"type\" value = \"pdf\" /> <b><font size=\"4\">Download as PDF</font></center></b>" +
+                        "<br>" + "\n" +
+                        "<center><input type=\"radio\" name=\"type\" value = \"latex\" /> <b><font size=\"4\">Download as LaTeX</font></center></b>" +
                         "<br><br><input type=\"submit\">" +
                         "</form>" +
                         "</p>");

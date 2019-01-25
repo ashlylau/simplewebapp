a67f67dda349f451ebe17d89f8c79aecf7e36169
pm3617 <pm3617@ic.ac.uk>
2019-01-25 12:29:38 +0000

    added test for pdf using both Mockito and Junit, but there's still a build error

--- src/main/java/ic/doc/web/PdfPage.java
+++ src/main/java/ic/doc/web/PdfPage.java
@@ -1,26 +1,26 @@
 package ic.doc.web;
 
-import javax.servlet.http.HttpServletResponse;
+import static ic.doc.WebServer.TMP_DIR;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import static ic.doc.WebServer.TMP_DIR;
+import javax.servlet.http.HttpServletResponse;
 
 public class PdfPage implements Page {
   
   @Override
   public void writeTo(HttpServletResponse resp) throws IOException {
     resp.setContentType("application/pdf");
-    
+
     getPdf();
     Path path = Paths.get(TMP_DIR + "/result.pdf");
     Files.copy(path, resp.getOutputStream());
     Files.delete(path);
   }
 
-  private void getPdf() {
+  public void getPdf() {
     ProcessBuilder pb = new ProcessBuilder("pandoc", TMP_DIR + "/result.md", "-s", "-o", TMP_DIR + "/result.pdf");
     final Process p;
     
